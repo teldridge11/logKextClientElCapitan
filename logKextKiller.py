a5d4b2c42de29b7b98c73b652aedbd292adbf131
Tom Eldridge <tom.eldridge@hotmail.com>
2015-12-21 00:54:21 -0500

    Update logKextKiller.py

--- logKextKiller.py
+++ logKextKiller.py
@@ -2,15 +2,17 @@ import subprocess
 import time
 import daemon
 
+# Loop to kill the key logger daemon
 def logKextClientKillLoop():
-    '''Kill the keylogger daemon.'''
     while True:
         subprocess.call(['pkill', 'logKextDaemon'])
         time.sleep(3600)
 
+# Run the kill loop with dameon context
 def run():
     with daemon.DaemonContext():
         logKextClientKillLoop()
 
+# If name is 'main' run the kill loop
 if __name__ == "__main__":
     run()
